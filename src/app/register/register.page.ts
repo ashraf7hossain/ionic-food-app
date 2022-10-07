3c04b1b79e31c927a69643499e5174c544a366db
irfan-rumon <irfannisho8571@gmail.com>
2022-10-07 20:22:24 +0600

    Implement feature of registering new user

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/user';
+import { UserApiService } from 'src/app/services/user-api.service';
+
 
 @Component({
   selector: 'app-register',
@@ -7,9 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
-  constructor() { }
+  user:User = {} as User;
+  passNotMatched: boolean = false;
 
-  ngOnInit() {
+  constructor(private router: Router, 
+              private userApi: UserApiService) { }
+
+  ngOnInit(): void {
   }
+
+  onSubmit(){
+     if( this.user.password != this.user.passConfirm)
+     {
+        this.passNotMatched = true;
+       
+        return;
+     }
+     this.userApi.addUser(this.user).subscribe(  (res)=>{
+         this.router.navigate(['/login']);
+     } )
+  }
+
+
 
 }
